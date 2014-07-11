e6a31213781a6400cf7a1584cc9e83a7a94cd149
David Pinto <david.pinto@bioch.ox.ac.uk>
2014-07-11 01:48:13 +0100

    fastSPDMome: intial version of web interface to localization code.
    This is not meant for batch processing. This is for optimization of imaging
    parameters during the imaging.
    
    * fastSPDMome.html: HTML form that submits image and options to the CGI script.
    * fastSPDMome.pl: CGI script that downloads imagem, creates Matlab code, forks
    Matlab process, and displays localized output.

---
+++ www/fastSPDMome.pl
@@ -0,0 +1,333 @@
+#!/usr/bin/perl -T
+use utf8;
+
+## Copyright (C) 2014 David Pinto <david.pinto@bioch.ox.ac.uk>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU Affero General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+## A perl CGI script to get an image from one of the microscopes, run
+## Matlab's code (Rainer's fastSPDMome), and display it on the web browser.
+## Batch processing will be implemented later on via Omero, this is only so
+## that users can see what they are imaging while adjusting parameters.
+##
+## This requires large files to be uploaded but the generated images, the ones
+## sent back to the user for display, should be below 1MB. An alternative would
+## be to make users move them into /micron1, and give them the a list of files
+## to select from.
+
+use strict;
+use warnings;
+use POSIX ();     # for &WNOHANG XXX consider replacing with IPC::Run
+use IPC::Open2;   # XXX consider replacing with IPC::Run
+use File::Temp;
+use File::Spec;
+use File::Basename;
+use Graphics::Magick; # for tiff->jpg conversion (firefox does not display tiff)
+use CGI; # XXX consider replacing with Dancer or Mojolicious::Lite
+
+## We are running in taint mode so we must set PATH ourselves. Since, we are
+## specifying the absolute path to Matlab, we could just clear PATH.
+## Unfortunately, the Matlab command is actually a bash script which will need
+## the following in the path.
+$ENV{PATH} = join(":", qw(/bin /usr/bin));
+
+##
+## Temporary file configuration
+##
+
+## The script will create temporary directories (one for each input image),
+## inside a general directory to which the web server user (www-data in Debian)
+## has write permissions. This script can remove the original data (the large
+## files), but not the generated images so they are available for download.
+## These generated images are very small but after some time... better have a
+## cron job that removes old files from there frequently.
+
+## In the filesystem, all files will be in $tmp_root/$tmp_webroot/tempname/
+## For the webserver, they will be in /$tmp_webroot/tempname/
+my $tmp_webroot = "fastSPDMome";
+my $tmp_root = "/tmp";
+
+## On the file system
+my $tmp_dir = File::Temp::tempdir(
+  DIR => File::Spec->catfile($tmp_root, $tmp_webroot),
+);
+## On the webserver
+my $tmp_webdir = File::Spec->catfile(
+  $tmp_webroot,
+  (File::Spec->splitdir ($tmp_dir))[-1]
+);
+
+##
+## CGI configuration
+##
+
+## Instead of a simple "500 server error" page, also display the actual error
+## message to the browser (we kinda trust the users) and add a message to
+## contact micron.
+use CGI::Carp qw(fatalsToBrowser);
+CGI::Carp::set_message <<"END";
+<p>
+Please contact David Pinto from Micron via phone on extension 13359 or
+mail <a href="mailto:david.pinto\@bioch.ox.ac.uk">david.pinto\@bioch.ox.ac.uk</a>.
+</p>
+<p>
+Please keep this error message and the value '$tmp_webdir' for reference.
+</p>
+END
+$CGI::POST_MAX = 1024 * 1000 * 200; # max 200MB file uploads
+
+##
+## Computation/Matlab configuration
+##
+
+my $matlab_path = '/usr/local/MATLAB/R2010b/bin/matlab';
+my @matlab_args = ('-nodisplay', '-nosplash'); # consider -nojvm in the future
+my $fastSPDMome_path = '/home/carandraug/dstorm-tst/locmic/';
+
+## Maximum time allowed (in seconds) before killing the process.
+## This is more than just the time given to Matlab. Even if we give Matlab
+## more time, the web client will time out give up on the connection, and the
+## the web server timeout our CGI script (default value for Apache in Debian
+## is 300sec). Still, all test images thus far take between 2-3min so we should
+## be safe.
+my $max_time = 4 * 60;
+
+##
+## Get parameters via CGI
+##
+my $cgi = new CGI;
+
+sub untaint_file {
+  ## We cannot use the file where it is after upload because the Matlab code
+  ## will create other files on the same directory. We could change the Matlab
+  ## code to save files somewhere else but we would still have to sanitize
+  ## the file names before passing them to Matlab. That would imply moving the
+  ## file which we can't do since it's open, so we really have to copy them.
+  my $name = shift;
+  die ("File too large for upload")
+    if ! defined $cgi->param($name);
+
+  my $fh = $cgi->upload($name);
+  die ("Failed to upload image")
+    if ! $fh;
+
+  ## The filename will be the most vulnerable thing added to the Matlab code.
+  ## The only character that could end a string and inject some extra commands
+  ## is the quote. That needs to go. However, we are communicating with Matlab
+  ## via pipes (see comments below why). This means that a newline could cause
+  ## a syntax error, which would cancel further evaluation of code, and
+  ## anything after would be evaluated alone, on its own line. So we will just
+  ## play safe and keep only the characters we know to be safe.
+  my $file_name = (File::Spec->splitpath($cgi->param($name)))[2];
+  $file_name =~ s/[^a-z0-9_\-+=\[\]:;"!%() \.]//gi;
+
+  ## Using tempfile means we don't have to worry about removing it in
+  ## the end but adds random string to the filename.
+  my ($tmpfh, $tmpfile) = File::Temp::tempfile(
+    $file_name . "XXXX",  # template must have at least 4 X
+    SUFFIX  => '.tif',
+    DIR     => $tmp_dir,
+    UNLINK  => 1,         # make sure this file is removed when we exit
+  );
+  print {$tmpfh} $_ while (<$fh>);
+  close $tmpfh;
+  return $tmpfile;
+}
+
+sub untaint_float {
+  ## This comes from a text field so we must use a regexp to untaint it. The
+  ## following will matches '2', '02', '2.00', and '.2' with optional
+  ## whitespace around it.
+  my $in = $cgi->param(shift);
+  $in =~ m/^\s*   # allow but do not capture prepad whitespace
+              (   # start of captured group
+               ([0-9]+(\.[0-9]*)?)  # '2', '02', or '2.00' (but not '2.')
+               |                    # or
+               (\.[0-9]+)           # '.02'
+              )   # end of captured group
+           \s*$   # allow but do not capture postpad whitespace
+          /x;
+  return $1;
+}
+
+sub untaint_bool {
+  ## These come from a checkbox. If defined they were selected.
+  my $in = $cgi->param(shift);
+  return defined $in ? "true" : "false";
+}
+
+my $image_path      = untaint_file  ('data');
+my $gain_correction = untaint_float ('gc');
+my $pixel_size      = untaint_float ('px');
+my $plot_distance   = untaint_bool  ('plotd');
+my $filter1         = untaint_bool  ('filter1'); # correction for long-lasting fluorescence
+my $filter2         = untaint_bool  ('filter2'); # correction for blinking fluorophores
+
+###
+### Generate Matlab code
+###
+
+my $code = <<END;
+status = 0;
+try
+  rmpath ('/usr/local/MATLAB/R2010b/locmic/fastSPDM');
+  rmpath ('/usr/local/MATLAB/R2010b/locmic/ox');
+  rmpath ('/usr/local/MATLAB/R2010b/locmic');
+  addpath ('$fastSPDMome_path');
+  fastSPDMome ('$image_path', $gain_correction, $filter1, $filter2, $pixel_size, $plot_distance);
+catch err
+  disp (['error: ' err.message()]);
+  disp (err.stack ());
+  status = 1;
+end
+exit (status);
+END
+
+##
+## Call Matlab
+##
+## Why are we forking a process and communicating over pipes, instead of
+## just using "system(..., '-r', command)"? Because:
+##    1) '-r' is really not like "--eval". Because the session persists after
+##    the command, if there's any syntax error such as:
+##            try, foo ('bar' bad-nonescaped-quote'); catch, end, exit (1);
+##    there is no catching and the process will hang forever waiting for input.
+##    2) the matlab command is actually a bash script and seems to do funny
+##    (read, weird) things when there are newlines. I do not know the full
+##    extent of things that may break '-r'.
+##    3) using pipes makes it much easier to set a maximum allowed time for
+##    computation before giving up (which at the moment is most likely caused
+##    by the matlab process waiting for input after error instead of exiting).
+##    4) I guess in the future, if processing becomes slower, this script will
+##    just start the process and return the session. This is closer to that.
+
+sub run_matlab {
+  ## We wish we could just use open() to write to Matlab but Matlab does not
+  ## have a silent option so we need to redirect its STDOUT somewhere. Because
+  ## of that we are using IPC::Open2::open2.
+
+  ## We won't really plan on using this, but sincewe have to redirect it, we
+  ## might as well it to somewhere useful in the future instead of devnull.
+  open(my $matlab_stdout, ">", File::Spec->catfile($tmp_dir, "matlab.stdout"))
+    or die("Could not open file for Matlab's STDOUT");
+
+  my $pid = IPC::Open2::open2($matlab_stdout, my $pipe,
+                              $matlab_path, @matlab_args);
+
+  print {$pipe} $code;
+
+  my $p_time    =  0;   # how much time has passed
+  my $interval  = 10;   # time interval (seconds) to check process
+  my $overtime  =  0;   # has it exceeded the allowed time?
+  my $running   =  1;   # is the process still running?
+  do {
+    sleep ($interval);
+    $p_time += $interval;
+    ## waitpid returns the process pid after it dies
+    $running  = waitpid ($pid, POSIX::WNOHANG) != $pid;
+    $overtime = $p_time > $max_time;
+  } until (! $running || $overtime);
+
+  die ("error running Matlab: $!")
+    if $? != 0;
+
+  if ($overtime) {
+    kill "SIGTERM", $pid;
+    die ("Computation was taking too long so we stopped it.");
+  }
+}
+
+sub get_output_files {
+  ## FIXME  we should probably make this an argument to the Matlab code or we
+  ##        run the risk of not getting the right thing one day. We only check
+  ##        for tif files with "_imstres_pz_" on their name. However, because
+  ##        the input file may already have it on the name, we check all of
+  ##        them and return the one with it near the end. The temporary
+  ##        directory should only have the original files plus the generated
+  ##        images which have the same basename appended with:
+  ##          * [fname '_positions.txt']
+  ##          * [fname '_impoints_pz_' num2str(pxlsz) '.tif']
+  ##          * [fname '_imstres_pz_' num2str(pxlsz) '.tif']
+  ##          * [fname '_locmic_log.txt']
+  ##          * [fname '_Distances.tif']
+
+  opendir (my $dirh, $tmp_dir)
+    or die $!;
+  my @fnames = readdir ($dirh);
+  close ($dirh);
+
+  my %files;
+  foreach my $key ("imstres", "log") {
+    ## Get file name with the key closer to the end of the filename
+    my %rind = map {$_ => rindex ($_, $key)} @fnames;
+    $files{$key} = (sort {$rind{$b} <=> $rind{$a}} @fnames)[0];
+  }
+  return %files;
+}
+
+run_matlab();
+my %output = get_output_files();
+
+##
+## Create HTML to display results
+##
+
+sub slurp_log {
+  ## This either returns the whole log (small file) in a single string,
+  ## or an empty string if it failed to open the file.
+  my $fpath = shift;
+  my $log = "";
+  if (open (my $fh, $fpath)) {
+    $log = do {local $/; <$fh>};
+  }
+  return $log;
+}
+
+sub get_display {
+  ## Not all browsers will be able to display the tiff generated, we need
+  ## to convert it into something else such as jpg or png.
+  my $tiff_name = shift;
+  my $tiff_path = File::Spec->catfile($tmp_dir, $tiff_name);
+  my $tiff_web  = File::Spec->catfile("/", $tmp_webdir, $tiff_name);
+  my $jpeg_name = $tiff_name . ".jpg";
+  my $jpeg_path = $tiff_path . ".jpg";
+  my $jpeg_web  = $tiff_web  . ".jpg";
+
+  my $image = Graphics::Magick->new;
+  ## Something bad happened if any of the methods returns non-zero
+  if (! $image->Read($tiff_path)
+      and ! $image->Write($jpeg_path)) {
+    return $cgi->a({href => $tiff_web}, $cgi->img({-src => $jpeg_web}));
+  } else {
+    return $cgi->a({href => $tiff_web}, "Conversion to JPEG failed. Download TIFF.");
+  }
+}
+
+sub center_pre {
+  return $cgi->p($cgi->table({-style=>"display: inline-table;text-align:left;"},
+    $cgi->Tr([$cgi->td($cgi->pre(shift))])
+  ));
+}
+
+print $cgi->header();
+print $cgi->start_html(-title => 'localized', -BGCOLOR => "#353535");
+
+print $cgi->div({-style=>"text-align:center;color:#CCCCCC"},
+  center_pre(slurp_log (File::Spec->catfile($tmp_dir, $output{'log'}))),
+  get_display($output{'imstres'}),
+  center_pre($code),
+);
+
+print $cgi->end_html();
+
